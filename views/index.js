bb90757379059e65c5cfe222ab8564b1c9384a9d
Samwise12 <sfelder1@bingahmton.edu>
2017-08-22 12:18:57 -0400

    first

--- views/index.js
+++ views/index.js
@@ -1,18 +1,27 @@
 import ReactDOM from 'react-dom';
 import React, {Component} from 'react';
+import random from 'lodash/random';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk'
+import { enableBatching } from 'redux-batched-actions';
+import { Provider } from 'react-redux';
 
+import './styles/app.scss';
+import App from './containers/app'
+import reducers from './reducers';
 
-class App extends Component {
-	constructor(props){
-		super(props);
-		this.state = {
+const store = createStore(
+	enableBatching( reducers ),
+	compose(
+	applyMiddleware(thunk)//,
+	//window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+		)
+	);
+// store.dispatch(createLevel(1));
+// store.dispatch(setDungeonLevel(1));
 
-		}
-	}
-	render(){
-		return(<div>asf</div>)
-	}
-}
-
-ReactDOM.render(<App />,
+ReactDOM.render(
+	<Provider store = {store}>
+		<App />
+	</Provider>,
 	document.getElementById('root'));
